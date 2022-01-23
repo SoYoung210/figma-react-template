21192e73713030c806bd2d5aa226e3a69a135932
SoYoung210 <ethdud1@gmail.com>
2022-01-23 15:23:42 +0900

    build: fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,22 +4,18 @@ const InlineChunkHtmlPlugin = require('react-dev-utils/InlineChunkHtmlPlugin');
 const path = require('path');
 
 module.exports = (env, argv) => ({
-  mode: argv.mode === 'production' ? 'production' : 'development',
-
   // This is necessary because Figma's 'eval' works differently than normal eval
   devtool: argv.mode === 'production' ? false : 'inline-source-map',
 
   entry: {
-    ui: './src/index.tsx', // The entry point for your UI code
-    code: './src/code.ts', // The entry point for your plugin code
+    ui: './src/index.tsx',
+    code: './src/code.ts',
   },
 
   module: {
     rules: [
-      // Converts TypeScript code to JavaScript
+      // TODO: babel-loader
       { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ },
-
-      // Enables including CSS by doing "import './file.css'" in your TypeScript code
       {
         test: /\.css$/,
         use: [
@@ -38,23 +34,20 @@ module.exports = (env, argv) => ({
       },
     ],
   },
-
-  // Webpack tries these extensions for you if you omit the extension like "import './file'"
   resolve: { extensions: ['.tsx', '.ts', '.jsx', '.js'] },
 
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'), // Compile into a folder called "dist"
+    path: path.resolve(__dirname, 'dist'),
   },
-
-  // Tells Webpack to generate "ui.html" and to inline "ui.ts" into it
   plugins: [
     new HtmlWebpackPlugin({
-      inject: 'body',
       template: './public/ui.html',
       filename: 'ui.html',
       inlineSource: '.(js)$',
       chunks: ['ui'],
+      inject: 'body',
+      cache: 'false'
     }),
     new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/ui/]),
   ],
